9e181123c7a830d0790423981a840b88af327e9d
FrankCostelliJr <frank.costelli@outlook.com>
2021-04-30 17:25:04 -0400

    more env fixes

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -7,7 +7,7 @@ import formatReservationTime from "./format-reservation-date";
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "starter-restaurant-reservation-backend-pi.vercel.app";
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
